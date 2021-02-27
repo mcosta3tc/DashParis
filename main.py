1db0fedf2f90cbaf1da96b415724081cbe3ff1f0
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-02-27 14:40:15 +0100

    pie chart added

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import numpy as np
-
 
 toilettes = pd.read_csv("data/sanisettesparis.csv", sep=';')
+
 
 '''
 # Toilettes publiques de Paris
@@ -12,32 +11,26 @@ toilettes = pd.read_csv("data/sanisettesparis.csv", sep=';')
 
 # get all toilettes
 total = toilettes.count().TYPE
-st.write("Actuellement il y a plus de ", total ," toilettes publiques dans Paris")
-
-#create lat and lon column base on 'geo_point_2d' as float
+st.write("Actuellement il y a plus de ", total, " toilettes publiques dans Paris")
+# create lat and lon column base on 'geo_point_2d' as float
 toilettes["lat"] = toilettes['geo_point_2d'].str.split(",").str.get(0).astype(float)
 toilettes["lon"] = toilettes['geo_point_2d'].str.split(",").str.get(1).astype(float)
-st.map(toilettes)
+st.map(toilettes, zoom=11)
 
-#create arr column base on arrondissement as "1, 18, 5.."
+# create arr column base on arrondissement as "1, 18, 5.."
 arrondissements = toilettes['ARRONDISSEMENT'].astype(str)
 arrondissements = arrondissements.str.replace("7500", "")
 arrondissements = arrondissements.str.replace("750", "")
 toilettes["arr"] = arrondissements
 toilettes["arr"] = toilettes["arr"].astype(int)
 
-st.write(toilettes)
+# st.write(toilettes[toilettes["arr"] == 5])
 
-
-#st.write(toilettes["Latitude"])
-#st.write(toilettes["Longitude"])
-
-
-
-#status = toilettes.groupby(pd.Grouper(key="ARRONDISSEMENT")).mean().round(0)
-#st.write(status)
-
-
-
-#fig = px.bar(toilettes, x='ARRONDISSEMENT', y='STATUT', orientation='h')
-#st.write(fig)
+#Pie chart types of toilettes by arrondissement
+pieType = px.pie(toilettes,
+                 values='arr',
+                 names='TYPE',
+                 hover_data=['arr'], labels={'arr':'Arrondissements'},
+                 color_discrete_sequence=px.colors.sequential.OrRd)
+pieType.update_traces(textposition='inside', textinfo='percent+label')
+st.plotly_chart(pieType)
