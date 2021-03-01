bf398d6927d66255921d0460adafeff87d84d2a9
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-03-01 17:06:36 +0100

    UI updatded bar charts added

--- main.py
+++ main.py
@@ -21,49 +21,78 @@ def get_data():
 
 data = get_data()
 
-'''
-# Toilettes publiques de Paris
-'''
+# SIDEBAR
+st.sidebar.title("Choisir un arrondissement")
+arr_to_filter = st.sidebar.slider('Du 1er au 20e arrondissement de Paris', 1, 20, 5)
+st.sidebar.write("Vous êtes dans le ", arr_to_filter, "arrondissement de Paris")
+# filter data
+filtered_data = data[data['arr'] == arr_to_filter]
+# filtered map
+st.sidebar.map(
+    filtered_data,
+    zoom=11.3
+)
 
-# get all toilettes
+# MAIN CONTAINER
+
+st.title("Les toilettes publiques à Paris")
+
+# count of toilets
 total = data.count().TYPE
 st.write("Actuellement il y a plus de ", total, " toilettes publiques dans Paris :toilet:")
 
-with st.beta_container():
-    col1, col2 = st.beta_columns([5, 1])
+# Expander: image, txt, map
+with st.beta_expander("En savoir plus"):
+    # define equals column
+    col1, col2 = st.beta_columns([3, 3])
     with col1:
-        # filter data
-        arr_to_filter = st.slider('Choisir un arrondissement', 1, 20, 5)
-        filtered_data = data[data['arr'] == arr_to_filter]
-        # display a map
-        st.map(
-            filtered_data,
-            zoom=12.3
-        )
+        st.image("https://cdn.paris.fr/paris/2019/10/10/huge-646b1a47f1e5c66d4f269e59db5ae0a8.jpg")
     with col2:
-        # Pie chart types of toilettes by arrondissement
-        pieType = px.pie(
-            filtered_data,
-            title='Moyennes des types toilettes présentes sur cet arrondissement',
-            values='arr',
-            names='TYPE',
-            hover_data=['arr'], labels={'arr': 'Arrondissement ', 'TYPE': 'Type de Toilette '},
-            color_discrete_sequence=px.colors.sequential.OrRd
-        )
-        pieType.update_traces(textposition='inside', textinfo='percent+label')
-        pieType.update_layout(showlegend=False)
-        st.plotly_chart(pieType)
+        st.markdown("""
+                     Plus de 750 toilettes publiques et urinoirs sont installés dans Paris pour répondre aux exigences de la vie parisienne et des touristes. Leur accès est gratuit sur tout le territoire parisien. [En savoir plus] (https://www.paris.fr/pages/les-sanisettes-2396) 
+                 """)
 
-        # Expander with image and text
-    with st.beta_expander("En savoir plus"):
-        col1, col2 = st.beta_columns([3, 3])
-        with col1:
-            st.image("https://cdn.paris.fr/paris/2019/10/10/huge-646b1a47f1e5c66d4f269e59db5ae0a8.jpg")
-        with col2:
-            st.write("""
-                 Plus de 750 toilettes publiques et urinoirs sont installés dans Paris pour répondre aux exigences de la vie parisienne et des touristes. Leur accès est gratuit sur tout le territoire parisien. [En savoir plus] (https://www.paris.fr/pages/les-sanisettes-2396) 
-             """)
+    # general map of toilets in Paris
+    # st.map(
+    #     data,
+    #     zoom=11,
+    #     use_container_width=False
+    # )
 
-st.write(filtered_data)
+st.subheader("Type de toilettes")
+# Pie chart types of toilettes by arrondissement
+pieType = px.pie(
+    filtered_data,
+    values='arr',
+    names='TYPE',
+    hover_data=['arr'],
+    labels={'arr': 'Arrondissement ', 'TYPE': 'Type de Toilette '},
+    color_discrete_sequence=px.colors.sequential.OrRd
+)
+pieType.update_traces(textposition='inside', textinfo='percent+label')
+st.plotly_chart(pieType)
 
-st.write(filtered_data.groupby("TYPE").count())
+# st.write(filtered_data)
+
+# Bar chart on statut
+st.subheader("Disponibilités")
+statut_group = filtered_data.groupby("STATUT").count()
+bar = px.bar(statut_group,
+             x=statut_group.index,
+             y='TYPE',
+             text='TYPE',
+             labels={'TYPE': '', 'STATUT': ''}
+             )
+st.plotly_chart(bar)
+
+# Bar chart on PMR
+st.subheader("Accessibles pour les personnes à mobilité réduite")
+acces_pmr = filtered_data.groupby("ACCES_PMR").count()
+bar = px.bar(acces_pmr,
+             x=acces_pmr.index,
+             y='TYPE',
+             text='TYPE',
+             labels={'TYPE': '', 'ACCES_PMR': ''}
+             )
+st.plotly_chart(bar)
+
